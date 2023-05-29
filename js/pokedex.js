3f5aa7a5eb13cf39cb404ab87ce48690eabe8d77
JuanmaLucas13 <jmmjeins@gmail.com>
2023-05-29 19:12:23 +0200

    correccion numero pokemons

--- js/pokedex.js
+++ js/pokedex.js
@@ -314,7 +314,7 @@ async function getDetailPokemons(pokemons)
 
 async function getPokemons()
  { 
-    const res = await fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=30") 
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=150") 
 	const resPokemons = await res.json(); 
 	getDetailPokemons(resPokemons.results); 
  } 
